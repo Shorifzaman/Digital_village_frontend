d3e5ac2c004205f1fb08150c76b84ca1e2119725
shorifzaman <shorif.zaman@gmail.com>
2023-06-12 01:09:49 +0600

    Error fix and ready to deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-// export const BASE_URI = 'https://digital-village.herokuapp.com';
+import axios from "axios";
+// export const BASE_URI = "https://plain-clam-tights.cyclic.app";
 export const BASE_URI = 'http://localhost:5000';
 
 export default axios.create({
@@ -8,6 +8,6 @@ export default axios.create({
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URI,
-  headers: { 'Content-Type': 'application/json' },
+  headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
